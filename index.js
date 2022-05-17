4043d7f59915c083377320a1e2ff8da659f8344c
Daniel Häggström <danielhaggstrom@hotmail.se>
2022-05-17 18:14:00 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Global variables
 const API_KEY = "6097562d-5f91-462b-b5e9-a941856f3fd4";
 const button = document.querySelector("#position-button");
 const locationlistElem = document.querySelector("#location-list");
@@ -7,18 +8,34 @@ const cardOverlay = document.querySelector("#times-overlay");
 const API_TOKEN =
   "pk.eyJ1Ijoiam9oYW5raXZpIiwiYSI6ImNrcnl6M25xMDA4aWUyd3BqY3EzYnA1NTEifQ.ve5rEn8ZDwUGKvphMkEdpw";
 
+// Gets my current location
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const long = pos.coords.longitude;
+      const lat = pos.coords.latitude;
+
+      // Calls getLongLat with my current coordinates
+      getLongLat(long, lat);
+    });
+  }
+}
+
+// Fetching for nearby bus-stops
 const getLongLat = async (long, lat) => {
   const res = await fetch(
     `https://api.resrobot.se/v2.1/location.nearbystops?format=json&accessId=${API_KEY}&originCoordLat=${lat}&originCoordLong=${long}`
   );
   const data = await res.json();
 
+  // Getting just the id and name of the stop
   const locationList = data.stopLocationOrCoordLocation.map((item) => {
     return { name: item.StopLocation.name, id: item.StopLocation.extId };
   });
 
   console.log(locationList);
 
+  // Calls to print out the locations
   printLocations(locationList);
 };
 
@@ -33,16 +50,20 @@ function printLocations(locationList) {
     .join("");
 }
 
+// Fetching the times and stop location from API
 async function getTimeTable(id) {
+  // Fetching with specific ID
   const res = await fetch(
     `https://api.resrobot.se/v2.1/departureBoard?id=${id}&format=json&accessId=${API_KEY}`
   );
   const data = await res.json();
 
+  // Opens time-modal
   cardOverlay.style.display = "flex";
 
   let newList = [];
 
+  // Only getting the first 10 times from bus-stop
   for (let i = 0; i < 10; i++) {
     newList.push({
       time: data.Departure[i].time.substring(0, 5),
@@ -53,6 +74,7 @@ async function getTimeTable(id) {
 
   console.log(newList);
 
+  // Writes out the Bus, last stop and departure time
   departureTimes.innerHTML = newList
     .map((item) => {
       return `<li>${item.name} mot ${item.direction}, ${item.time}</li>`;
@@ -60,23 +82,17 @@ async function getTimeTable(id) {
     .join("");
 }
 
+// Closes the modal when "gray-area" is clicked
 function closeModal(e) {
   if (e.target == e.currentTarget) {
     cardOverlay.style.display = "none";
   }
 }
 
+// Makes sure website is fully loaded before any events happens
 window.addEventListener("load", () => {
-  button.addEventListener("click", () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        const long = pos.coords.longitude;
-        const lat = pos.coords.latitude;
-
-        getLongLat(long, lat);
-      });
-    }
-  });
-
+  // gets your current location
+  button.addEventListener("click", getLocation);
+  // closes the time-modal
   cardOverlay.addEventListener("click", closeModal);
 });
